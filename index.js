09b26d89085021cbeeb2690832bc8ce483aaef2e
Veronika <nikazemlakova38@gmail.com>
2023-06-13 00:48:36 +0300

    simple_numbers_search

---
+++ index.js
@@ -0,0 +1,17 @@
+const M = 5;
+const N = 40;
+
+for (let i = M; i <= N; i++) {
+    let flag = 1
+    for (let j = 2; j <= N; j++) {
+        if ((i % j == 0) && (i != j))
+            flag = 0
+    }
+    if (flag == 1) {
+        console.log(i)
+    }
+}
+ 
+       
+
+
